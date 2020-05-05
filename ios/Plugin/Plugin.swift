c5348392e499769ea27a023b72166a05b922c22d
rex.chen <rex.chen@appier.com>
2020-05-05 15:24:15 +0800

    update qgsdk

--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -1,15 +1,17 @@
 import Foundation
 import Capacitor
-
+import HeyRexLib
 /**
  * Please read the Capacitor iOS Plugin Development Guide
  * here: https://capacitor.ionicframework.com/docs/plugins/ios
  */
 @objc(HeyAiquaIonicPlugin)
 public class HeyAiquaIonicPlugin: CAPPlugin {
-    
+
     @objc func echo(_ call: CAPPluginCall) {
         let value = call.getString("value") ?? ""
+
+        QGSdk.getSharedInstance().logEvent("233")
         call.success([
             "value": value
         ])
