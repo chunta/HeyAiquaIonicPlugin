bfc1916392f89037735c8ba6a849ad5193135afb
rex.chen <rex.chen@appier.com>
2020-07-20 17:34:52 +0800

    0.1.5

--- ios/Plugin/Plugin.m
+++ ios/Plugin/Plugin.m
@@ -4,8 +4,21 @@
 // Define the plugin using the CAP_PLUGIN Macro, and
 // each method the plugin supports using the CAP_PLUGIN_METHOD macro.
 CAP_PLUGIN(HeyAiquaIonicPlugin, "HeyAiquaIonicPlugin",
-           CAP_PLUGIN_METHOD(echo, CAPPluginReturnNone);
-           CAP_PLUGIN_METHOD(start, CAPPluginReturnPromise);
-           CAP_PLUGIN_METHOD(log, CAPPluginReturnPromise);
-           CAP_PLUGIN_METHOD(setToken, CAPPluginReturnPromise);
+       CAP_PLUGIN_METHOD(configure, CAPPluginReturnPromise);
+       CAP_PLUGIN_METHOD(setUniversalLinkDomains, CAPPluginReturnPromise);
+       CAP_PLUGIN_METHOD(logEvent, CAPPluginReturnPromise);
+       CAP_PLUGIN_METHOD(flush, CAPPluginReturnPromise);
+       CAP_PLUGIN_METHOD(setUserId, CAPPluginReturnPromise);
+       CAP_PLUGIN_METHOD(setName, CAPPluginReturnPromise);
+       CAP_PLUGIN_METHOD(setFirstName, CAPPluginReturnPromise);
+       CAP_PLUGIN_METHOD(setLastName, CAPPluginReturnPromise);
+       CAP_PLUGIN_METHOD(setCity, CAPPluginReturnPromise);
+       CAP_PLUGIN_METHOD(setEmail, CAPPluginReturnPromise);
+       CAP_PLUGIN_METHOD(setPhoneNumber, CAPPluginReturnPromise);
+       CAP_PLUGIN_METHOD(setDayOfBirth, CAPPluginReturnPromise);
+       CAP_PLUGIN_METHOD(setMonthOfBirth, CAPPluginReturnPromise);
+       CAP_PLUGIN_METHOD(setYearOfBirth, CAPPluginReturnPromise);
+       CAP_PLUGIN_METHOD(setCustomKey, CAPPluginReturnPromise);
+       CAP_PLUGIN_METHOD(setAttributionWindow, CAPPluginReturnPromise);
+       CAP_PLUGIN_METHOD(setClickAttributionWindow, CAPPluginReturnPromise);
 )
--- src/web.ts
+++ src/web.ts
@@ -9,22 +9,70 @@ export class HeyAiquaIonicPluginWeb extends WebPlugin implements HeyAiquaIonicPl
     });
   }
 
-  async echo(_: { value: string }): Promise<void> {
-    //return options;
-    //console.log(value);
-    // return;
+  // init sdk
+  async configure(_: {
+    appId: string;
+    senderId?: string;
+    appGroup?: string;
+    isDev?: boolean;
+  }): Promise<void> {
+    /* do nothing. */
   }
 
-  async start(options: { appid: string }): Promise<{appid: string}> {
-    return options;
+  async setUniversalLinkDomains(_: { domains: string[] }): Promise<void> {
+    /* do nothing. */
   }
 
-  async log(options: { name: string }): Promise<{name: string}> {
-    return options;
+  async logEvent(_: {
+    eventName: string;
+    parameters?: unknown;
+    vts?: number;
+    vtsCurr?: string;
+  }): Promise<void> {
+    /* do nothing. */
+  }
+  async flush(): Promise<void> {
+    /* do nothing. */
+  }
+  async setUserId(_: { userId: string }): Promise<void> {
+    /* do nothing. */
+  }
+  async setName(_: { name: string }): Promise<void> {
+    /* do nothing. */
+  }
+  async setFirstName(_: { firstname: string }): Promise<void> {
+    /* do nothing. */
+  }
+  async setLastName(_: { lastname: string }): Promise<void> {
+    /* do nothing. */
+  }
+  async setCity(_: { city: string }): Promise<void> {
+    /* do nothing. */
+  }
+  async setEmail(_: { email: string }): Promise<void> {
+    /* do nothing. */
+  }
+  async setPhoneNumber(_: { phoneNo: string }): Promise<void> {
+    /* do nothing. */
+  }
+  async setDayOfBirth(_: { day: number }): Promise<void> {
+    /* do nothing. */
+  }
+  async setMonthOfBirth(_: { month: number }): Promise<void> {
+    /* do nothing. */
+  }
+  async setYearOfBirth(_: { year: number }): Promise<void> {
+    /* do nothing. */
+  }
+  async setCustomKey(_: { key: string; value: unknown }): Promise<void> {
+    /* do nothing. */
   }
 
-  async setToken(options: { token: string }): Promise<{token: string}> {
-    return options;
+  async setAttributionWindow(_: { seconds: number }): Promise<void> {
+    /* do nothing. */
+  }
+  async setClickAttributionWindow(_: { seconds: number }): Promise<void> {
+    /* do nothing. */
   }
 }
 
