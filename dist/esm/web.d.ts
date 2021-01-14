6d3e99a8731a5229a20f7d58ae24ec284df15dca
rex.appier <rex.chen@appier.com>
2021-01-14 17:11:20 +0800

    0.3.34

--- dist/esm/web.d.ts
+++ dist/esm/web.d.ts
@@ -65,7 +65,7 @@ export declare class HeyAiquaIonicPluginWeb extends WebPlugin implements HeyAiqu
         scenarioId: string;
         productId?: string;
         parameters?: unknown;
-    }): Promise<void>;
+    }): Promise<unknown>;
 }
 declare const HeyAiquaIonicPlugin: HeyAiquaIonicPluginWeb;
 export { HeyAiquaIonicPlugin };
