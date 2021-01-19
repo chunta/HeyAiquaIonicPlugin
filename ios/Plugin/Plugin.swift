dff966ef9e9c0aec46e15116242bcfce57a99b64
rex.appier <rex.chen@appier.com>
2021-01-19 17:23:56 +0800

    add more ///

--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -5,7 +5,7 @@ import Appier
 @objc(HeyAiquaIonicPlugin)
 public class HeyAiquaIonicPlugin: CAPPlugin {
 
-    /// HeyAiqua Official //
+    /// HeyAiqua Official /////
     @objc func configure(_ call: CAPPluginCall) {
         guard let appId = call.options["appId"] as? String else {
             call.reject("Missing AppId")
